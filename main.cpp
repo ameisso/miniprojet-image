819f9076c827db3d61a3a2c4b4c4df96302b812f
Antoine Meissonnier <ameisso@gmail.com>
2013-12-04 17:36:38 +0100

    j'ai suivit la méthode jusqu'a la soustraction du fond. Prochaine étape détecter les groupes de personnes et ajouter les objets blocs au vecteur

--- main.cpp
+++ main.cpp
@@ -1,25 +1,116 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-
-
 #include <cstdio>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
-
-
+//*************************************************************************
+//Variables
+int imageIndex=0; //index de l'image que l'on est en train de traiter.
+int studiedLine=100;//numéro de la ligne de matrice que l'on étudie
+int studiedLineWidth=10;//nombre de lignes vers le bas par rapport à studiedLine que l'on prend en compte.
+std::string path;//répertoire de travail
+//image de référence qui sert à extraire le fond...
+//pour le moment c'est toujours la même, ensuite, on pourra toujours la modifier avec les autres images.
+Mat refImg=imread("data/detection_0000.jpeg");
+Mat currentImg;//image que l'on est en train d'analyser
+Mat extractedLine;//ligne extraite de l'image courante.
+Mat backgroundLine; //correspond aux lignes de l'image de fond que l'on étudie.
+Mat extractedLineNoBackground;//matrice de l'image sans fond.
+//*************************************************************************
+//Déclarations des fonctions
+bool checkIfNewImage(int lastImageIndex);
+string toString(int val);
+void extractLine(Mat img,Mat &extractedLine,int lineNumber, int lineWidth);
+void substractBackground(Mat refImg,Mat CurrentImg, Mat OutputImg);
+//*************************************************************************
 int main()
 {
-    Mat lastImage;//derière image chargé dans le projet -> celle que l'on va traiter
-    cout<<"ca marche"<<endl;
-    lastImage = imread("data/detection_0200.jpeg", 1);
-    if(lastImage.empty() )
+    //on vérifie que l'image de fond existe (benêt, benêt, benêt!!!)
+    if(refImg.empty() )
     {
-        cout << "Couldn't open image "<<endl;
-        return 0;
+        cout << "Couldn't open background image "<<endl;
+        return false;
     }
-    namedWindow("org");
-    imshow("org",lastImage);
-    cvWaitKey();
+    imshow("image de fond",refImg);
+    cvWaitKey(1000);//on affiche l'image de fond 1s pour vérifier
+    //on extrait de l'image de fond les lignes qui nous intéresse (pour la soustraction).
+    extractLine(refImg,backgroundLine,studiedLine,studiedLineWidth);
+    imshow("image de fond",backgroundLine);
+    while (1)
+    {
+        if(checkIfNewImage(imageIndex))
+        {
+            currentImg=imread(path);
+            extractLine(currentImg,extractedLine,studiedLine,studiedLineWidth);
+            imshow("extractedLines",extractedLine);
+            substractBackground(backgroundLine,extractedLine,extractedLineNoBackground);
+            cvWaitKey(5);
+            //Si on trouve une nouvelle image, on peut faire le traitement. c'est ici que ca commence.
+            imageIndex++;
+        }
+    }
+}
+
+
+bool checkIfNewImage(int lastImageIndex)
+{
+    //fonction qui va regarder dans le répertoire data si il y a une nouvelle image
+    if (lastImageIndex<10)
+    {
+        path="data/detection_000"+toString(lastImageIndex)+".jpeg";
+    }
+    else if (lastImageIndex<100)
+    {
+        path="data/detection_00"+toString(lastImageIndex)+".jpeg";
+    }
+    else if (lastImageIndex<1000)
+    {
+        path="data/detection_0"+toString(lastImageIndex)+".jpeg";
+    }
+    Mat img  = imread(path, 1);
+    if(img.empty() )
+    {
+        cout << "Couldn't open image "<<lastImageIndex<<endl;
+        cvWaitKey();
+        return false;
+    }
+    else
+    {
+        //cout<<"image :"<<path<<" found"<<endl;
+        return true;
+    }
+}
+//Fonction qui retourne la valeur d'un INT sous forme de chaine de charactère.
+string toString(int val)
+{
+    stringstream ss;
+    ss<<val;
+    string str=ss.str();
+    return str;
+}
+//Fonction qui extrait la ligne que l'on analyse  de l'image courante. ...
+//Dans la version deux, on pourra toujours appeller plusieure fois cette fonction pour extraire plusieures lignes.
+void extractLine(Mat img,Mat &extractedLine,int lineNumber, int lineWidth)
+{
+    if((lineNumber+lineWidth<img.rows)||(lineNumber+lineWidth<img.rows))
+    {
+        extractedLine=img.rowRange(lineNumber,lineNumber+lineWidth);
+    }
+    else
+    {
+        // si on essaye d'extraire une ligne de matrice qui n'existe pas dans la matrice d'origine
+        //on affiche un message d'erreur et on attend 1000ms.
+        cout<<"Line to extract out of bound, max line is :"<<img.rows<<endl;
+        cvWaitKey(1000);
+    }
+}
+//Fonction qui soustrait le fond de l'image à l'image courante.
+//refImg est l'image de base (sans pietons que l'on a choisis au début.)
+void substractBackground(Mat refImg,Mat CurrentImg, Mat OutputImg)
+{
+        Mat diff;
+        absdiff(CurrentImg,refImg,OutputImg);
+        imshow("Image Without Background",OutputImg);
 }
