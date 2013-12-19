2c96bc92f63cd8b696fea6578ba1f2ff1daff8a8
tommag <magnier.tom@gmail.com>
2013-12-19 11:45:18 +0100

    Ajout de la classe BackgroundSubtractorAvg (soustraction de fond par moyenne glissante)

---
+++ backgroundsubtractoravg.cpp
@@ -0,0 +1,16 @@
+#include "backgroundsubtractoravg.h"
+
+BackgroundSubtractorAvg::BackgroundSubtractorAvg()
+{
+
+}
+
+void BackgroundSubtractorAvg::operator()(InputArray image, OutputArray fgmask, double learningRate)
+{
+
+}
+
+void BackgroundSubtractorAvg::getBackgroundImage(OutputArray bgImage) const
+{
+
+}
